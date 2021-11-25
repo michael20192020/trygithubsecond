496f2c31fed6aecb65efdfdcedc47c23da920437
michael20192020 <zhuqi2012@hotmail.com>
2021-11-26 09:36:43 +1100

    add bindview function

--- app/src/main/java/com/hansoft/trygithubsecond/MainActivity.java
+++ app/src/main/java/com/hansoft/trygithubsecond/MainActivity.java
@@ -3,12 +3,28 @@ package com.hansoft.trygithubsecond;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView titleTextView;
+    private EditText nameEditText;
+    private Button submitButton;
+    private Button clearButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindview();
     }
+
+    private void bindview()
+    {
+        titleTextView = findViewById(R.id.titleTextView);
+        submitButton = findViewById(R.id.submitButton);
+        clearButton = findViewById(R.id.clearButton);
+    }
+
 }
